9a576e456d46ae77325b774223ed08d73778650f
mikejeffers <mike.s.jeffers@gmail.com>
2017-03-16 11:18:23 -0400

    new nn impl WIP; and fix math bug on NN logic

--- src/main/java/edu/mscd/thesis/nn/MLPlocal.java
+++ src/main/java/edu/mscd/thesis/nn/MLPlocal.java
@@ -27,25 +27,36 @@ import edu.mscd.thesis.util.ModelStripper;
 import edu.mscd.thesis.util.Rules;
 import edu.mscd.thesis.util.Util;
 
-public class NeuralNet implements AI {
+public class MLPlocal implements AI {
+	/**
+	 * input layer is 9-cell neighborhood including center tile, and Moore's
+	 * Neighbors output layer is 1 value which is predictive Q-lookup of
+	 * eventual game-score as result of action on state score estimate will be
+	 * tile-based only, no city data
+	 */
 
 	public static final BasicNetwork network = new BasicNetwork();
 	public static final MLDataSet DATASET = new BasicMLDataSet();
 	private Model state;
-	private Model trueModel;
 	private Random random = new Random();
 
-	public NeuralNet(Model model) {
-		trueModel = model;
-		this.state =  ModelStripper.reducedCopy(model);
+	public MLPlocal(Model model) {
+		this.state = ModelStripper.reducedCopy(model);
 		initNetwork();
 		initTrainingDataSet();
 		trainBackProp();
 	}
-	
+
 	private void initTrainingDataSet(){
-		double[][] input = {this.getInputArrayFromWorld(state.getWorld())};
-		double[][] idealout = {{Rules.score(state)}};
+		//getInputAroundTile
+		Tile[] tiles = state.getWorld().getTiles();
+		
+		double[][] input = new double[tiles.length][9];
+		double[][] idealout = new double[tiles.length][1];
+		for(int i=0; i<input.length; i++){
+			input[i] = this.getInputAroundTile(state.getWorld(), tiles[i].getPos());
+			idealout[i]=new double[]{Rules.score(tiles[i])};
+		}
 		for (int i = 0; i < input.length; i++) {
 			MLData trainingIn = new BasicMLData(input[i]);
 			MLData idealOut = new BasicMLData(idealout[i]);
@@ -66,14 +77,15 @@ public class NeuralNet implements AI {
 
 		Encog.getInstance().shutdown();
 	}
-	
+
 	private void trainResilient() {
 		ResilientPropagation train = new ResilientPropagation(network, DATASET);
 		int epoch = 1;
 
 		do {
 			train.iteration();
-			//System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+			// System.out.println("Epoch #" + epoch + " Error:" +
+			// train.getError());
 			epoch++;
 		} while (train.getError() > 0.01 && epoch < 50);
 		train.finishTraining();
@@ -82,9 +94,9 @@ public class NeuralNet implements AI {
 	}
 
 	private void initNetwork() {
-		network.addLayer(new BasicLayer(null, true, state.getWorld().height() * state.getWorld().width()));
-		network.addLayer(new BasicLayer(new ActivationTANH(), true, 25));
-		network.addLayer(new BasicLayer(new ActivationTANH(), true, 15));
+		network.addLayer(new BasicLayer(null, true, 9));
+		network.addLayer(new BasicLayer(new ActivationTANH(), true, 16));
+		network.addLayer(new BasicLayer(new ActivationTANH(), true, 4));
 		network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 1));
 		network.getStructure().finalizeStructure();
 		network.reset();
@@ -106,11 +118,11 @@ public class NeuralNet implements AI {
 	@Override
 	public UserData takeNextAction() {
 		// TODO Auto-generated method stub
-		//evaluate a finite set of random actions
-		//pick one with highest value outcome
+		// evaluate a finite set of random actions
+		// pick one with highest value outcome
 		World w = this.state.getWorld();
 		Tile[] tiles = w.getTiles();
-		int possibleActions = tiles.length*4;
+		int possibleActions = tiles.length * 4;
 		Pos2D[] locations = new Pos2D[possibleActions];
 		ZoneType[] zTypes = new ZoneType[possibleActions];
 		double[] results = new double[possibleActions];
@@ -118,54 +130,47 @@ public class NeuralNet implements AI {
 		int minIndex = 0;
 		double maxScore = 0;
 		double minScore = Rules.MAX;
-		
-		for(int i=0 ;i<tiles.length; i++){
-			int zoneCounter=0;
+		for (int i = 0; i < tiles.length; i++) {
+			int zoneCounter = 0;
 			Pos2D pos = tiles[i].getPos();
-			for(ZoneType zt: ZoneType.values()){
-				int index = i*4+zoneCounter;
+			for (ZoneType zt : ZoneType.values()) {
+				int index = i + zoneCounter;
 				locations[index] = pos;
 				zTypes[index] = zt;
+				System.out.println(Arrays.toString(zTypes));
 				w.setAllZonesAround(locations[index], zTypes[index], 1, true);
 				results[index] = getOutput(getInputArrayFromWorld(w));
-				if(results[index]>maxScore){
+				if (results[index] > maxScore) {
 					maxScore = results[index];
-					maxIndex=index;
+					maxIndex = index;
 				}
-				if(results[index]<minScore){
-					minIndex=index;
+				if (results[index] < minScore) {
+					minIndex = index;
 					minScore = results[index];
 				}
 				zoneCounter++;
 			}
 		}
-		
-		
-		/* --Random--
-		for(int i=0; i<possibleActions; i++){
-			locations[i] = randomPos();
-			zTypes[i] = randomZone();
-			w.setAllZonesAround(locations[i], zTypes[i], 1, true);
-			results[i] = getOutput(getInputArrayFromWorld(w));
-			if(results[i]>maxScore){
-				maxScore = results[i];
-				maxIndex=i;
-			}
-			if(results[i]<minScore){
-				minIndex=i;
-				minScore = results[i];
-			}
-		}
-		*/
-		
-		System.out.println("Possible actions Score domain["+results[minIndex]+","+results[maxIndex]+"]");
+		System.out.println(Arrays.toString(locations));
+		System.out.println(Arrays.toString(zTypes));
+
+		/*
+		 * --Random-- for(int i=0; i<possibleActions; i++){ locations[i] =
+		 * randomPos(); zTypes[i] = randomZone();
+		 * w.setAllZonesAround(locations[i], zTypes[i], 1, true); results[i] =
+		 * getOutput(getInputArrayFromWorld(w)); if(results[i]>maxScore){
+		 * maxScore = results[i]; maxIndex=i; } if(results[i]<minScore){
+		 * minIndex=i; minScore = results[i]; } }
+		 */
+
+		System.out.println("Possible actions Score domain[" + results[minIndex] + "," + results[maxIndex] + "]");
 		System.out.print("Best move:{");
 		System.out.print(locations[maxIndex]);
-		System.out.println(" "+zTypes[maxIndex]);
+		System.out.println(" " + zTypes[maxIndex]);
 		System.out.print("Worst move:{");
 		System.out.print(locations[minIndex]);
-		System.out.println(" "+zTypes[minIndex]);
-		//Rules.score(state);
+		System.out.println(" " + zTypes[minIndex]);
+		// Rules.score(state);
 		UserData fake = new UserData();
 		fake.setClickLocation(locations[maxIndex]);
 		fake.setZoneSelection(zTypes[maxIndex]);
@@ -175,17 +180,16 @@ public class NeuralNet implements AI {
 		fake.setDrawFlag(true);
 		fake.setAI(true);
 		return fake;
-		
 
 	}
-	
-	private Pos2D randomPos(){
+
+	private Pos2D randomPos() {
 		int x = random.nextInt(state.getWorld().width());
 		int y = random.nextInt(state.getWorld().height());
 		return new Pos2D(x, y);
 	}
-	
-	private ZoneType randomZone(){
+
+	private ZoneType randomZone() {
 		return ZoneType.values()[random.nextInt(ZoneType.values().length)];
 	}
 
@@ -199,35 +203,51 @@ public class NeuralNet implements AI {
 		double currentScore = Rules.score(state);
 		double prevScore = Rules.score(prev);
 		MLData trainingIn = new BasicMLData(getInputArrayFromWorld(state.getWorld()));
-		MLData idealOut = new BasicMLData(new double[]{currentScore});
+		MLData idealOut = new BasicMLData(new double[] { currentScore });
 		DATASET.add(trainingIn, idealOut);
 		this.trainBackProp();
-		//this.trainResilient();
-		if(currentScore>prevScore){
-			System.out.println("AI move improvedScore! " +currentScore+" from "+prevScore);
-			
-		}else{
-			System.out.println("AI move dropped score =( " +currentScore+" from "+prevScore);
+		// this.trainResilient();
+		if (currentScore > prevScore) {
+			System.out.println("AI move improvedScore! " + currentScore + " from " + prevScore);
+
+		} else {
+			System.out.println("AI move dropped score =( " + currentScore + " from " + prevScore);
 		}
 
 	}
-	
-	private double[] getInputArrayFromWorld(World w){
-		Tile[] tiles = w.getTiles();
+
+	private double[] getInputAroundTile(World w, Pos2D p) {
+		Tile[] tiles = new Tile[9];
+		int index = 0;
+		for (int i = -1; i < 2; i++) {
+			for (int j = -1; j < 2; j++) {
+				Pos2D nLoc = new Pos2D(p.getX() + i, p.getY() + j);
+				tiles[index] = w.getTileAt(nLoc);
+				index++;
+			}
+		}
 		double[] vals = new double[tiles.length];
-		for(int i=0; i<tiles.length; i++){
+		for (int i = 0; i < tiles.length; i++) {
 			vals[i] = getInputValueFromTile(tiles[i]);
 		}
 		return vals;
 	}
 
+	private double[] getInputArrayFromWorld(World w) {
+		Tile[] tiles = w.getTiles();
+		double[] vals = new double[tiles.length];
+		for (int i = 0; i < tiles.length; i++) {
+			vals[i] = getInputValueFromTile(tiles[i]);
+		}
+		return vals;
+	}
 
 	private double getInputValueFromTile(Tile t) {
+		if (t == null) {
+			return 0;
+		}
 		double numTypes = ZoneType.values().length;
-		return t.getZoneType().ordinal()/numTypes;
+		return t.getZoneType().ordinal() / numTypes;
 	}
-	
-	
-	
 
 }
